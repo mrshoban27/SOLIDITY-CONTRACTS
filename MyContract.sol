03766427be37b2d5da0c431c35c98b0a37695cb5
mrshoban27 <37335034+mrshoban27@users.noreply.github.com>
2018-03-22 15:37:04 +0530

    Create MyContract.sol

---
+++ MyContract.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.20;
+contract MyContract{
+    address creator;
+    uint256 myNumber;
+    function MyContract(){
+        creator=msg.sender;
+        myNumber=3;
+    }
+    function getCreator()constant returns(address){
+        return creator;
+    }
+    function getMyNumber()constant returns(uint){
+        return myNumber;
+    }
+    function setMyNumber(uint256 myNewNumber){
+        myNumber=myNewNumber;
+    }
+    function kill(){
+        if(msg.sender==creator){
+            suicide(creator);
+        }
+    }
+}
